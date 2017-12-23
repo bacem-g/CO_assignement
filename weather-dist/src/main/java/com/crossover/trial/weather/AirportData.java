509390f11a3b135d976107cadd07c7523daed88b
Bacem_G <ghali.bacem@gmail.com>
2017-12-23 19:54:48 +0300

    first commit

---
+++ weather-dist/src/main/java/com/crossover/trial/weather/AirportData.java
@@ -0,0 +1,74 @@
+package com.crossover.trial.weather;
+
+import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+
+/**
+ * Basic airport information.
+ *
+ * @author code test administrator
+ */
+public class AirportData {
+
+    /** the three letter IATA code */
+    String iata;
+
+    /** latitude value in degrees */
+    double latitude;
+
+    /** longitude value in degrees */
+    double longitude;
+
+    public AirportData() { }
+    
+    public AirportData(String iata) {
+		super();
+		this.iata = iata;
+	}
+
+	public AirportData(String iata, double latitude, double longitude) {
+		super();
+		this.iata = iata;
+		this.latitude = latitude;
+		this.longitude = longitude;
+	}
+
+
+
+	public String getIata() {
+        return iata;
+    }
+
+    public void setIata(String iata) {
+        this.iata = iata;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    public boolean equals(Object other) {
+        if (other instanceof AirportData) {
+            return ((AirportData)other).getIata().equals(this.getIata());
+        }
+
+        return false;
+    }
+    
+    public String toString() {
+    	return super.toString();
+        //return ReflectionToStringBuilder.toString(this, ToStringStyle.NO_CLASS_NAME_STYLE);
+    }
+}
---
+++ weather-dist/src/main/java/com/crossover/trial/weather/AirportLoader.java
@@ -0,0 +1,72 @@
+package com.crossover.trial.weather;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+/**
+ * A simple airport loader which reads a file from disk and sends entries to the
+ * webservice
+ *
+ * 
+ * @author code test administrator
+ */
+public class AirportLoader {
+
+	/** end point for read queries */
+	private WebTarget query;
+
+	/** end point to supply updates */
+	private WebTarget collect;
+
+	public AirportLoader() {
+		Client client = ClientBuilder.newClient();
+		query = client.target("http://localhost:9090/query");
+		collect = client.target("http://localhost:9090/collect");
+	}
+
+	public void upload(InputStream airportDataStream) throws IOException {
+		BufferedReader reader = new BufferedReader(new InputStreamReader(airportDataStream));
+		String l = null;
+		String[] airportDataArray = new String[9];
+		List<AirportData> airportDataList = new ArrayList<>();
+		while ((l = reader.readLine()) != null) {
+			airportDataArray = l.split(",");
+			AirportData airportData = new AirportData(airportDataArray[4], Double.parseDouble(airportDataArray[6]),
+					Double.parseDouble(airportDataArray[7]));
+			airportDataList.add(airportData);
+			
+		}
+		
+//		for(AirportData ad: airportDataList) {
+//			WebTarget path = collect.path("/airport/"+ad.getIata()+"/"+ad.getLatitude()+"/"+ad.getLongitude());
+//			path.request().post(Entity.entity(null, MediaType.APPLICATION_JSON));
+//		}
+		WebTarget path = collect.path("/airports");
+		path.request().post(Entity.entity(airportDataList, MediaType.APPLICATION_JSON));
+	}
+
+	public static void main(String args[]) throws IOException {
+		File airportDataFile = new File(args[0]);
+		if (!airportDataFile.exists() || airportDataFile.length() == 0) {
+			System.err.println(airportDataFile + " is not a valid input");
+			System.exit(1);
+		}
+
+		AirportLoader al = new AirportLoader();
+		al.upload(new FileInputStream(airportDataFile));
+		System.exit(0);
+	}
+}
